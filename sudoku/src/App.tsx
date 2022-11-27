98aedd260d954e56cd584c48f156d017abf53f43
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-11-27 13:09:02 +1030

    validating the columns of the table

--- sudoku/src/App.tsx
+++ sudoku/src/App.tsx
@@ -4,30 +4,33 @@ import styles from './App.module.css';
 
 const App: Component = () => {
   let boardState = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    [7, 2, 6, 3, 5, 9, 4, 1, 8],
+    [4, 5, 8, 1, 6, 7, 2, 3, 9],
+    [9, 1, 3, 8, 2, 4, 7, 6, 5],
+    [1, 6, 2, 9, 7, 5, 3, 8, 4],
+    [3, 9, 4, 2, 8, 6, 1, 5, 7],
+    [8, 7, 5, 4, 1, 3, 9, 2, 6],
+    [5, 3, 7, 6, 4, 1, 8, 9, 2],
+    [6, 8, 9, 7, 3, 2, 5, 4, 1],
+    [2, 4, 1, 5, 9, 8, 6, 7, 3]
   ]
   function checkSoduku() {
     let validBoard = true
-
+    let validTotal = 45
     boardState.forEach((row, index) => {
-      // check each row 
+      let colResult = 0
+      // check each row total is 45
       let rowResult: number = row.reduce((total, currentValue) => total + currentValue)
-      // let colResult: number = 
+      if (rowResult != validTotal) validBoard = false
 
-      console.log(row)
-      if (rowResult < 45) validBoard = false
+      // check each col total is 45
+      row.forEach((val, subIndex) => {
+        colResult += boardState[subIndex][index]
+      })
+      if (colResult != validTotal) validBoard = false
+
+
     })
-    // check each col 
-
-    console.log(boardState[8][8])
     // check each box
 
     console.log(validBoard)
