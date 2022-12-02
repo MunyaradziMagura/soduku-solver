fa3b5a72a294d989a859ae14b584f529ff86ba89
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-12-02 15:02:51 +1030

    able to disable initial board state numbers and able to record key presses and positions

--- sudoku/src/Components/Board.jsx
+++ sudoku/src/Components/Board.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export default function Board({boardState}) {
+export default function Board({boardState, initialBoard}) {
+
   return (
     <table>
-    {boardState.map((element, index) => (
+    {initialBoard.map((element, index) => (
       <tr key={index} id={`row-${index}`}>
         {element.map((value, subIndex) => (
-          <td><input id={`cell-${index}-${subIndex}`} type="text" placeholder={value} maxLength='1' disabled={(value != 0) ? true : false}/></td>
+          <td id={`cell-${index}-${subIndex}`}><input type="text" placeholder={value} maxLength='1' onKeyUp={(e) => console.log({key: e.target.value, position: `Y = ${index} | X = ${subIndex}`})} disabled={(initialBoard[index][subIndex] != 0) ? true : false}/></td>
         ))}
       </tr>
     ))}
